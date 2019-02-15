3e59a51800c9d4afc82a465e617a789c43d430ab
Reza Majidi <rezamajidizavie@gmail.com>
2019-02-15 17:41:30 +0330

    add get users/me

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const _ = require("lodash");
 const { mongoose } = require("./db/mongoose");
 const { User } = require("./models/user");
 const { Todo } = require("./models/todo");
+let { authenticate } = require("./middleware/authenticate");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -93,6 +94,27 @@ app.patch("/todos/:id", (req, res) => {
     .catch(err => {
       res.status(400).send();
     });
+});
+
+app.post("/users", (req, res) => {
+  let body = _.pick(req.body, ["email", "password"]);
+  let user = new User(body);
+
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      res.header("x-auth", token).send(user);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
+app.get("/users/me", authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
